cdfe103ad0e3b1ab0301f82c5f44b5d4562f532b
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-06-18 16:07:59 +0800

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+	    erb :index #This tells Sinatra to render a file called index.erb inside of a directory called views
 	end
+
+	get "/info" do
+		erb :info
+		#It's important to note that the name of the file doesn't have to match the name of the route.
+		#By convention though, we keep our routes and our erb files named the same. This makes it easier to keep track of as our projects get bigger.
+	end
+
 end
